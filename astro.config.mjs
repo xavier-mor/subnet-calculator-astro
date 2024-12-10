057e976cd9c9e90d025c33aa6999adc566d8abcf
Xavier Mor <xaviermor@Xaviers-MacBook-Pro.local>
2024-12-11 00:38:48 +0200

    astro amplify support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import awsAmplify from 'astro-aws-amplify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://subnetcalculator.dev",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  adapter: awsAmplify(),
 });
