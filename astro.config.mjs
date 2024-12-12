feb4181dcbc349044de037199fae3fc9ee2be6b7
Xavier Mor <xaviermor@Xaviers-MacBook-Pro.local>
2024-12-12 15:41:01 +0200

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import awsAmplify from 'astro-aws-amplify';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://subnetcalculator.dev",
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  integrations: [
+    tailwind(), mdx(), sitemap(), icon(), partytown({ config: { forward: ['dataLayer.push'] } })
+  ],
   adapter: awsAmplify(),
-  output: 'server',
+  output: 'server'
 });
